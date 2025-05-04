2d4d586320a9d14f37afaf86bea87efdc697ac20
Priyanshu Kumarteotia <priyanshukumarteotia11@gmail.com>
2025-05-04 09:32:18 +0530

    Added JS notes for Day 1 and Day 2

---
+++ JS/APP.js
@@ -0,0 +1,200 @@
+// if(1) {
+//     console.log("it has true vale");
+// }
+// else {
+//  console.log("it has false value");
+// }
+// let color = "red";
+// switch(color) {
+//     case "blue" :
+//         console.log("stop");
+//         break;
+//         case "yellow" :
+//             console.log("slow down");
+//             break;
+//             case "green" :
+//             console.log("Go");
+//             break;
+//             default :
+//             console.log("Broken Light");
+// }
+// let number = "7";
+// switch(number) {
+//     case "1" :
+//         console.log("Monday");
+//         break;
+//         case "2" :
+//             console.log("Tuesday");
+//             break;
+//             case "3" :
+//                 console.log("Wednesday");
+//                 break;
+//                 case "4" :
+//                     console.log("Thrusday");
+//                     break;
+//                     case "5" :
+//                         console.log("Friday");
+//                         break;
+//                         case "6" :
+//                             console.log("Saturday");
+//                             break;
+//                             case "7" :
+//                                 console.log("Sunday");
+// }
+// alert("something is wrong!");
+// let firstname = prompt("enter your first name");
+// let lastname = prompt("enter your last name");
+// console.log("welcome", firstname," ", lastname);
+console.log("hello");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
