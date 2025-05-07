4f3c1821a443b34f25a61059e8c40305de0f672f
Priyanshu Kumarteotia <priyanshukumarteotia11@gmail.com>
2025-05-07 15:29:50 +0530

    JS section 11

---
+++ JS-12/app.js
@@ -0,0 +1,17 @@
+async function greet() {
+    throw "weak connection"
+    return "hello!";
+}
+
+greet()
+    .then((result) => {
+        console.log("promise was resolved");
+        console.log("result was", result);
+        
+        
+
+    })
+    .catch((err) => {
+        console.log("promise was rejected with err :",err);
+        
+    });
